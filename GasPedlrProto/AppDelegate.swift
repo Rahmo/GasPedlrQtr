271099439320330fcd5aae2a88ace74bd7a72aa5
Rahmo <rahmo.alzahrani@gmail.com>
2016-01-10 16:11:53 -0600

    2

--- GasPedlrProto/AppDelegate.swift
+++ GasPedlrProto/AppDelegate.swift
@@ -35,6 +35,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate,CLLocationManagerDelegate 
         PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
         
         
+        
+        
         locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
           MapViewController().refresh()
