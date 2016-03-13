d19d83286c243dbc54ec9db9df80486a1b144e18
Rahmo <rahmo.alzahrani@gmail.com>
2016-03-12 23:31:13 -0600

    rahmo pulls

--- GasPedlrProto/GameViewController.swift
+++ GasPedlrProto/GameViewController.swift
@@ -29,7 +29,7 @@ class GameViewController: UIViewController, MySceneDelegate {
             lifeLabel.text = String(_lifeCount)
         }
     }
-
+    
     func gameStart() {
         
         _lifeCount = 10
@@ -39,7 +39,7 @@ class GameViewController: UIViewController, MySceneDelegate {
         _myScene.reset()
         _readyToFire = true
     }
-
+    
     @IBAction func restart(_ : AnyObject) {
         _myScene.respawn(completion:{ self.gameStart() })
     }
@@ -77,7 +77,7 @@ class GameViewController: UIViewController, MySceneDelegate {
         let randomBlue:CGFloat = CGFloat(drand48())
         return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
     }
-
+    
     func setPadPosition(value:Float) {
         if _readyToFire {
             _myScene.fire()
@@ -111,7 +111,7 @@ class GameViewController: UIViewController, MySceneDelegate {
         
         _myScene = MyScene(size: skView.frame.size)
         skView.presentScene(_myScene)
-
+        
         padSlider.minimumValue = 0
         padSlider.maximumValue = Float(_myScene.size.width)
         padSlider.value = Float(_myScene.padX)
@@ -120,7 +120,7 @@ class GameViewController: UIViewController, MySceneDelegate {
         scheduledTimerWithTimeInterval()
         gameStart()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
